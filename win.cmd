4ad50dc6496f8dd555f59b0a0cb4967759e5bd5c
Antti Arrak <antti004@users.noreply.github.com>
2025-10-31 14:10:24 +0200

    Download win.ps1 and delete win.cmd

--- win.cmd
+++ win.cmd
@@ -2,3 +2,5 @@
 powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command ""Set-ExecutionPolicy -ExecutionPolicy Bypass""' -Verb RunAs}"
 
 powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest https://github.com/antti004/install/raw/main/win.ps1 -o win.ps1"
+
+del win.cmd
