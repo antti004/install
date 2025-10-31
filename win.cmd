7d133a2a1b2c1fa3af40fa429e3ec95b56d549d7
Antti Arrak <antti004@users.noreply.github.com>
2025-10-31 14:08:49 +0200

    win.cmd

---
+++ win.cmd
@@ -0,0 +1,4 @@
+@echo off
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command ""Set-ExecutionPolicy -ExecutionPolicy Bypass""' -Verb RunAs}"
+
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest https://github.com/antti004/install/raw/main/win.ps1 -o win.ps1"
